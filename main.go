66fcf0199e54c94db94944318474df1b14badf3a
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2025-10-20 09:52:06 -0300

    Uso de concurrencia con waigroups (#3)

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func printSomething(something string) {
+func printSomething(something string, waitgroup *sync.WaitGroup) {
+	// Notify the waitgroup that this goroutine is done when the function exits.
+	defer waitgroup.Done()
 	fmt.Println(something)
 }
 
@@ -13,10 +15,28 @@ func printSomething(something string) {
 // It's a go routine that runs when the program starts.
 func main() {
 	
-	go printSomething("This is the first thing to be printed.")
+	var waitgroup sync.WaitGroup
 
-	time.Sleep(1 * time.Second) // Add a sleep to allow the goroutine to complete before main exits.	
+	words := []string{"alpha", "beta", "gamma", "delta", "pi", "theta", "lambda", "omega"}
+
+	// Set the number of goroutines to wait for.
+	waitgroup.Add(len(words))
+
+	for index, word := range words {
+		go printSomething(fmt.Sprintf("Word %d: %s", index+1, word), &waitgroup)
+	}
+
+	waitgroup.Wait()
+
+	// Start a new goroutine to print the first message.
+	// go printSomething("This is the first thing to be printed.", &waitgroup)
+
+	// time.Sleep(1 * time.Second) // Add a sleep to allow the goroutine to complete before main exits.	
 								// It's generally not a good practice to use sleep for synchronization,
 								// but it's used here for simplicity in this example.
-	printSomething("This is the second thing to be printed.")
+
+	// Start a new goroutine to print the second message.
+	waitgroup.Add(1)
+
+	printSomething("This is the second thing to be printed.", &waitgroup)
 }
